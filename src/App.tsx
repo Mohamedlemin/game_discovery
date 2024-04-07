78d0586c2a0edcaacecc74639a2cbf2442112587
kdraa12 <med3744lemin@gmail.com>
2024-04-07 12:49:21 -0500

    color switcher

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,11 @@ function App() {
     <NavBar></NavBar>
   </GridItem>
   <Show above='lg'>
-  <GridItem pl='2' bg='pink.300' area={'aside'}>
+  <GridItem pl='2' area={'aside'}>
     aside
   </GridItem>
   </Show>
-  <GridItem pl='2' bg='green.300' area={'main'}>
+  <GridItem pl='2'  area={'main'}>
     Main
   </GridItem>
  
